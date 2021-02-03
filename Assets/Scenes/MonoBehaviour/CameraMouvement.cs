5286ec351eaa455d1f4e016fca2430c7bd33da70
Romain Lopez-Rostain <romain.lopez-rostain@etu.univ-lyon1.fr>
2021-02-04 00:34:46 +0100

    POC

---
+++ Assets/Scenes/MonoBehaviour/CameraMouvement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMouvement : MonoBehaviour
+{
+    private GameObject blob;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        blob = GameObject.Find("Blob");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mean_children_position = Vector3.zero;
+        foreach (Transform child in blob.transform)
+        {
+            GameObject obj = child.gameObject;
+            mean_children_position += obj.transform.position;
+        }
+        mean_children_position /= blob.transform.childCount;
+
+        this.gameObject.transform.position = mean_children_position;
+        this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y, -10) ;
+    }
+}
---
+++ Assets/Scenes/MonoBehaviour/Controls.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    public float max_size;
+    public float coef;
+    private Rigidbody2D rb2D;
+
+    void Start()
+    {
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+        max_size = 5;
+        coef = 0.09f;
+    }
+
+    void OnMouseDown()
+    {
+        // Reinit parameters
+        coef = 0.09f;
+    }
+
+    void OnMouseDrag()
+    {
+        coef = Mathf.Max(0.05f, coef - 0.001f);
+        Vector3 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mouse_pos - this.transform.position;
+
+        float magnitude = Mathf.Min(direction.magnitude, max_size);
+
+        rb2D.AddForce(coef * magnitude * direction.normalized, ForceMode2D.Impulse);
+        Debug.Log(this.gameObject.name);
+    }
+
+    #if true
+    void OnRenderObject()
+    {
+        Vector3 part_pos = this.transform.position;
+        Vector3 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mouse_pos - this.transform.position;
+
+        float magnitude = Mathf.Min(direction.magnitude, max_size);
+        mouse_pos = this.transform.position + direction.normalized * magnitude;
+
+        GL.Begin(GL.LINES);
+        GL.Color(new Color(1f, 1f, 1f, 1f));
+        GL.Vertex3(mouse_pos.x, mouse_pos.y, mouse_pos.z);
+        GL.Vertex3(part_pos.x, part_pos.y, part_pos.z);
+        GL.End();
+    }
+    #endif
+}
