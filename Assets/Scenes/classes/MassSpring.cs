d6919bad8db9ac12397c56e8c6f83f4bb79eb817
unknown <math.dumont.md@gmail.com>
2021-02-03 14:11:27 +0100

    Début MassSpring et classe Node

---
+++ Assets/Scenes/classes/MassSpring.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MassSpring : MonoBehaviour
+{
+    // nombre de particule
+    public int nb_particule = 40;
+
+    // Masse globale du système répartie équitablement entre les noeuds
+    public float mass = -1;
+
+    // Radius de la sphere d'une particule
+    public float radius = 1;
+
+    // fréquence des noeuds
+    public float frequency;
+
+    // damping ratio
+    public float damping;
+
+    public List<Node> nodes = new List<Node>();
+
+    public MassSpring()
+    {
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+                nodes.Add(new Node(radius, 1, 0.9f, 0.9f));
+        
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++)
+            {
+                
+            }
+    }
+}
---
+++ Assets/Scenes/classes/Node.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public GameObject sphere;
+
+    /*
+     * Liste des spring, le spring à l'indice i relie le Node *this* au node
+     * dans linked_nodes au meme indice, c'est-à-dire à l'indice i.
+     */
+    public List<SpringJoint2D> springs = new List<SpringJoint2D>();
+    public List<Node> linked_nodes = new List<Node>();
+
+    public Node(float radius, float mass, float drag, float angularDrag)
+    {
+        sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+        CircleCollider2D collider = sphere.AddComponent(typeof(CircleCollider2D)) as CircleCollider2D;
+        collider.radius = radius;
+
+        //https://www.reddit.com/r/Unity2D/comments/ki7a7b/friction_not_working_on_circle_collider_2d/
+        Rigidbody2D rigidbody = sphere.AddComponent(typeof(Rigidbody2D)) as Rigidbody2D;
+        rigidbody.mass = mass;
+        rigidbody.drag = drag;
+        rigidbody.angularDrag = angularDrag;
+
+    }
+
+    public void link(Node other, SpringJoint2D spring)
+    {
+        springs.Add(spring);
+        linked_nodes.Add(other);
+    }
+
+    /* 
+     * Dans unlink, on supprime le spring et le node associé dans la liste,
+     * et on appelle unlink sur le node associé
+     */
+    public void unlink(SpringJoint2D spring)
+    {
+        int idx = -1;
+        for (int i = 0; i < springs.Count; i++)
+            if (spring == springs[i])
+            {
+                idx = i;
+                break;
+            }
+
+        if (idx != -1)
+        {
+            springs.RemoveAt(idx);
+            linked_nodes[idx].unlink(spring);
+            linked_nodes.RemoveAt(idx);
+            Destroy(spring);
+        }
+    }
+
+}
