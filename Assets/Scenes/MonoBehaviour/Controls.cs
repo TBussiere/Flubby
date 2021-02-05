71e93adbdb00b530ea937832542972fe6903b6da
unknown <math.dumont.md@gmail.com>
2021-02-05 16:13:31 +0100

    deux possiblites pour controle, a voir

--- Assets/Scenes/MonoBehaviour/Controls.cs
+++ Assets/Scenes/MonoBehaviour/Controls.cs
@@ -31,13 +31,21 @@ public class Controls : MonoBehaviour
         float time_elapsed = Mathf.Min(Time.time - when_clicked, delta_t);
         coef = coef_max - (coef_max - coef_min) * (time_elapsed / delta_t);
         Vector3 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 drag_vector = mouse_pos - this.transform.position;
-        float drag_magnitude = drag_vector.magnitude;
-        Vector3 drag_direction = drag_vector.normalized;
-        // float magnitude = Mathf.Min(direction.magnitude, max_drag_distance);
-        float magnitude = Mathf.Min(drag_magnitude, fmax);
+        Vector3 direction = mouse_pos - this.transform.position;
 
-        rb2D.AddForce(magnitude * drag_direction, ForceMode2D.Impulse);
+        float magnitude = Mathf.Min(direction.magnitude, max_size);
+
+        rb2D.AddForce(coef * magnitude * direction.normalized, ForceMode2D.Impulse);
+
+        // Vector3 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Vector3 drag_vector = mouse_pos - this.transform.position;
+
+        // float drag_magnitude = drag_vector.magnitude;
+        // Vector3 drag_direction = drag_vector.normalized;
+        // // float magnitude = Mathf.Min(direction.magnitude, max_drag_distance);
+        // float magnitude = Mathf.Min(drag_magnitude, fmax);
+
+        // rb2D.AddForce(magnitude * drag_direction, ForceMode2D.Impulse);
         // Debug.Log(this.gameObject.name);
     }
 
